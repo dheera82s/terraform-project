1242bad34c0a18545555e0fcf8ee4f361328c5a1
dheera82s <610anilkumarb@gmail.com>
2026-05-22 21:36:57 +0530

    add terraform create_before_destroy

---
+++ create_before_destroy/main.tf
@@ -0,0 +1,22 @@
+#create_before_destroy
+#like already running the ec2 instance, nexr change the ami id then run an dbefire ec2 automatically deleted now new inmstance creted
+
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "usa-instance" {
+
+  ami = "ami-0236922087fa98b6e"
+
+  instance_type = "t3.micro"
+  tags = {
+    Name = "usa-instance"
+  }
+}
+
+lifecycle {
+  create_before_destroy = true
+}   
